d88aa01126eddd33818e51c0b566fd992c2db083
DulceZavala2621 <74272706+DulceZavala2621@users.noreply.github.com>
2022-10-13 22:54:56 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Car from "./Car.js"
 export default function App() {
 
   return (
-    <div >{/* esto es un comentario*/}
+    <div 
+    style = {{ display: "flex", flexFlow: "column", justifyContent: "center", alignItems: "center",}}>{/* esto es un comentario*/}
       <h1 >Hello StackBlitz!</h1>
-      <p>Aqui va la descripcion del carro :)</p>
+      <p>:p</p>
       <Car marca = "Corvette C6" imagen="https://static.retail.autofact.cl/blog/c_img_740x370.s12ttab8kzre30dn.jpg"
       Descripcion ="rojo y rapido"/>
       <Car marca = "Chevrolet L-170"imagen="https://i.pinimg.com/474x/91/2e/d4/912ed4ffcee5c4e018f7ad412cfac861.jpg"
--- src/Car.js
+++ src/Car.js
@@ -2,26 +2,39 @@ import React from "react";
 import "./style.css";
 
 export default function Car(props) {
+   //Style from: https://www.w3schools.com/howto/howto_css_cards.asp
+   //https://github.com/fjml1983/react-intro/blob/main/src/Car.js
   const estiloDivCard = {
-    boxshadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
- };
- const estiloDivCard = {
-  with: "100%",
-};
- const estiloH2 = {
-    color: "green"
-};
-const estiloH1 = {
-  color: "red"
-};
-  return (
-  
-    <div style= {estiloDivCard}>{/* esto es un comentario*/}
-      <h2 style= {estiloH2}>im car modelo: {props.marca}</h2>
-      
-      <img src={props.imagen}  alt= "Imagen carro" width="400"/>
+    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+    borderRadius: '30px',
+    maxWidth: '350px',
+    marging: 'auto',
+    backgroundColor: "cyan",
+  };
 
-      <p>{props.Descripcion}</p>
+  const estiloImgCard = {
+    width: '100%',
+    borderRadius: '10px 5px 0 0',
+  };
+
+  const estiloDivCardContenedor = {
+    padding: '0px 16px 16px',
+  };
+
+  return (
+    <div style={props.style}>
+      <div style={estiloDivCard}>
+        <img style={estiloImgCard} src={props.imagen} alt="Imagen del carro" />
+        <div style={estiloDivCardContenedor}>
+          <h2>{props.nombre}</h2>
+          <p>
+            <b>Descripción:</b> {props.Descripcion}
+          </p>
+          <em>
+            <b>Modelo:</b> {props. marca}
+          </em>
+        </div>
+      </div>
     </div>
   );
 }
